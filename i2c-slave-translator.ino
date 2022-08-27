b7ce4d994f8b1cc0f58ead4ba242aac9dcc292aa
Rhein <112055313+coprolitebbs@users.noreply.github.com>
2022-08-27 13:16:54 +1000

    Add files via upload

--- i2c-slave-translator.ino
+++ i2c-slave-translator.ino
@@ -6,7 +6,9 @@
 #define SLAVE_ADDR 0x08
 
 // Define Slave answer size
-#define ANSWERSIZE 5
+#define RDBUFFERLEN 1
+
+//#define USE_SOFT_SERIAL 1
 
 //Для Serial-порта выбраны отдельные пины, реализующие SoftSerial-порт
 //Это сделано для возможности отладки через основной Serial-порт
@@ -17,15 +19,15 @@
 //Если HIGH – модем посылает, мы их читаем отсюда с SoftSerial
 #define PIN_ACCESS 9
 
-
+#ifdef USE_SOFT_SERIAL
 SoftwareSerial Sserial(RS232_RX_PIN, RS232_TX_PIN);  // (Rx, Tx)
+#endif
 
-//String answer = "Hello";
 bool DataReaded = false;
 bool DataNeedOut = false;
 String ReadedData = "";
 String OutData = "";
-char rec[256];
+uint8_t rec[RDBUFFERLEN];
 byte reclen = 0;
 byte recptr = 0;
 
@@ -36,62 +38,77 @@ bool locked = false;
 //String receiveBuffer = "";
 byte lastch = 0;
 
-//Чтение по i2c прилетевшего байта и сразу отправка его в Serial-порт модема. 
+//Чтение по i2c прилетевшего байта и сразу отправка его в Serial-порт модема.
 //Все равно, при этом событии он простаивает
-void receiveEvent(int numBytes) { 
+void receiveEvent(int numBytes) {
   PORTB |= _BV(1);
-  while (Wire.available()){
+  while (Wire.available()) {
     byte x = Wire.read();
+#ifdef USE_SOFT_SERIAL
     Sserial.write((char)x);
+#else
+    Serial.write((char)x);
+#endif
   }
 }
 
-//Отправка по запросу с Master'a на шину i2с байт с буфера считанных с Serial модема данных 
-//Отправляется один байт, за ним байт флага. Если байт последний в буфере, то, флаг = 0
+//Отправка по запросу с Master'a на шину i2с байт с буфера считанных с Serial модема данных
+//Отправляется один байт, за ним байт флага. Если байт последний в буфере, то, флаг = 0, иначе 1
+//Если же данных с модема вообще небыло, вернем флаг = 2
 void requestEvent() {
-  byte resp = 0;//lastch;
-  byte atr = 1;
-  //if ((DataReaded) && (!locked)) {
+  uint8_t pack[2];
+  pack[0] = 0;
+  pack[2] = 2;
+  //byte resp = 0;
+  //byte atr = 2;  //Атрибут байта установим по умолчанию в 2. Это значит, что с модема ничего не читалось,
+  //и нам нечего возвращать на шину I2C компьютеру
   PORTB |= _BV(1);
   if (DataReaded) {
-    resp = (byte)rec[recptr];
-    recptr++;
+    /*resp*/pack[0] = (uint8_t)rec[recptr++];
+    pack[1] = 1;
     if (recptr == reclen) {
       DataReaded = false;
       reclen = 0;
-      recptr = 0;
-      atr = 0;
+      recptr = 0;      
+      pack[1] = 0;
     }
-  }  
-  Wire.write(resp);
-  Wire.write(atr);  
+  }
+  Wire.write(pack,2);
+  delayMicroseconds(1);
 }
 
 
 
 //Цикличная функция чтения с порта модема в буфер накопленных байт.
-//Поскольку, на прием-передачу Serial прерываний не предусмотрено, 
-//могут возникать ситуации, что при рассинхронизации готовности к передачи модема и готовности 
+//Поскольку, на прием-передачу Serial прерываний не предусмотрено,
+//могут возникать ситуации, что при рассинхронизации готовности к передачи модема и готовности
 //к приему данной платы-транслятора окна приема и передачи могут расходиться.
-//Опытным путем установлено, что чем меньше этот буфер, тем стабильнее работает прием-передача,
+//Опытным путем установлено, что чем меньше этот буфер (RDBUFFERLEN), тем стабильнее работает прием-передача,
 //и байты не теряются.
 void readData() {
   //byte sz = 0;
   if (!DataReaded) {
     locked = true;
     reclen = 0;
-    
+    //Устанавливаем в HIGH пин разрешения записи в порт для модема
     //digitalWrite(PIN_ACCESS, HIGH);
-    //Устанавливаем в HIGH пин разрешения записи в порт для модема 
     PORTB &= ~(_BV(1));
-    //int mx = Sserial.available();
-    //if (mx > 255) mx = 255;
-    while ((Sserial.available() > 0) && (reclen < 2)) {
-      rec[reclen] = Sserial.read();
-      reclen++;
+#ifdef USE_SOFT_SERIAL
+    //while ((Sserial.available() > 0) && (reclen <= RDBUFFERLEN)) {
+    while ((Sserial.available() > 0) && (reclen <= RDBUFFERLEN)) {
+      rec[reclen++] = Sserial.read();
+      //reclen++;
+      delayMicroseconds(5);
     }
+#else
+    delayMicroseconds(10);
+      while (Serial.available() && (reclen < RDBUFFERLEN)) {
+        rec[reclen++] = Serial.read();
+        //delayMicroseconds(2);
+      }
+#endif
+    //Устанавливаем в LOW пин разрешения записи в порт для модема
     //digitalWrite(PIN_ACCESS, LOW);
-    //Устанавливаем в LOW пин разрешения записи в порт для модема 
     PORTB |= _BV(1);
     if (reclen > 0) {
       recptr = 0;
@@ -102,35 +119,25 @@ void readData() {
 }
 
 
+#ifdef USE_SOFT_SERIAL
+
 //Вывод в Serial для отладки
 void printData() {
   PORTB |= _BV(1);
   if (DataReaded) {
-    //Serial.print(ReadedData);
-    //delay(10);
     for (byte i = 0; i < reclen; i++) {
       Serial.write((char)rec[i]);
     }
     DataReaded = false;
-    //ReadedData = "";
-    //reclen = 0;
-    //delay(2);
   }
 }
 
 //Чтение с Serial для отладки
 void getSerial() {
-  //byte sz = 0;
   PORTB |= _BV(1);
   outreclen = 0;
   outrecptr = 0;
-  //int mx = Serial.available();
-  //if (mx > 255) mx = 255;
   while ((Serial.available() > 0) && (outreclen < 255)) {
-    //for (int i = 0; i < mx; i++) {
-    //char rd = Serial.read();
-    //OutData = OutData + rd;
-    //sz++;
     outrec[outreclen] = Serial.read();
     outreclen++;
   }
@@ -138,30 +145,23 @@ void getSerial() {
 
 }
 
-
 //Отправка считанного в SoftSerial для отладки
 void outData() {
   PORTB |= _BV(1);
   locked = true;
   if (DataNeedOut) {
-    /*for (int i = 0; i < OutData.length(); i++) {
-      Sserial.write(OutData.charAt(i));
-      }*/
     for (int i = 0; i < outreclen; i++) {
       Sserial.write((char)outrec[i]);
     }
     //Sserial.print(OutData);
     DataNeedOut = false;
     outreclen = 0;
-
-    //OutData = "";
-    //delay(2);
   }
   locked = false;
 }
 
 
-
+#endif
 
 
 
@@ -184,15 +184,19 @@ void setup() {
   Wire.onReceive(receiveEvent);
 
   // Setup Serial Monitor
-  Sserial.begin(2400);
-  Serial.begin(9600);
+#ifdef USE_SOFT_SERIAL
+  Sserial.begin(4800);
+#endif
+  Serial.begin(4800/*,SERIAL_8E2*/);
   delay(500);
 }
 
 void loop() {
   readData();
-  //printData();
-  //getSerial();
-  //outData();
+#ifdef USE_SOFT_SERIAL
+  printData();
+  getSerial();
+  outData();
+#endif
 
 }
